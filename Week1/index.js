7c7099c91756c415308272397c753adc25737700
pdhakal688 <pdhakal688@gmail.com>
2025-10-03 11:46:11 +0000

    submitted

--- Week1/index.js
+++ Week1/index.js
@@ -1,1 +1,28 @@
+console.log("hello, world");
+const num1 = 5;
+const num2 = 3;
 
+// add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+
+const prompt = require(`prompt-sync`)();
+console.log("starting")
+const name = prompt(`Enter your name: `);
+console.log(`Hello, ${name}`);
+
+const number = parseInt(prompt("Enter a number: "));
+
+if (number > 0) {
+    console.log("The number is positive");
+}
+
+else if (number = 0) {
+    console.log("The number is zero");
+}
+
+else {
+    console.log("The number is negative");
+}
